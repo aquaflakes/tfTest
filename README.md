5d0be96978f64d9e920f4c7b2c841e55237abb9d
zhu <aquaflakes@gmail.com>
2018-06-27 09:47:12 +0100

    v1

--- README.md
+++
@@ -1,2 +0,0 @@
-# tfTest
-# test git
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-# tt
+from pyensembl import EnsemblRelease
+
+# release 77 uses human reference genome GRCh38
+data = EnsemblRelease(77)
+
+# will return ['HLA-A']
+
+
+
+# get all exons associated with HLA-A
+# exon_ids  = data .exon_ids_of_gene_name('HLA-A')ct.download()
+data.download()
+data.index()
+gene_names = data.gene_names_at_locus(contig=6, position=29945884)
+
+print (2)
